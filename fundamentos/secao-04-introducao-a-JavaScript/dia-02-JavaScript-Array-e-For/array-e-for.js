ad86ec2e4956ba58c7c03b9d757aa40869184950
Amanda Aragão <amandaaragao72@gmail.com>
2022-12-08 13:34:17 -0300

    exercicio de Arrays e for

---
+++ fundamentos/secao-04-introducao-a-JavaScript/dia-02-JavaScript-Array-e-For/array-e-for.js
@@ -0,0 +1,17 @@
+// Percorra o array imprimindo todos os valores nele contidos com a função console.log();
+
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+console.log(numbers);
+
+// Some todos os valores contidos no array e imprima o resultado;
+
+let contaTotal=0;
+for(i= 0; i < numbers.length; i+=1){
+ contaTotal = contaTotal + numbers[i];
+}   
+console.log(contaTotal,);
+
+// Calcule e imprima a média aritmética dos valores contidos no array;
+
+
+
